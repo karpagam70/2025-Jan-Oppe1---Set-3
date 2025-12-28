0d36715fb3a5699d4dc73eb4c7c849bd784c3e42
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:39:15 +0530

    Create Question 3.py

---
+++ Question 3.py
@@ -0,0 +1,13 @@
+'''
+Capitalize nth Character
+You are given a string s and an integer n. Your task is to capitalize the nth character in the string s, If n is greater than the length of the string, return the string unchanged.
+
+Examples
+
+>>> upper_nth_index_char("hello", 1)
+'hEllo'
+>>> upper_nth_index_char("python", 3)
+'pytHon'
+>>> upper_nth_index_char("python", 9)
+'python'
+'''
