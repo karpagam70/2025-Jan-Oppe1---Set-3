073e1f1d069630c37a6088a655a00ac1c7f4f3e7
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:36:52 +0530

    Create Question 1.py

---
+++ Question 1.py
@@ -0,0 +1,15 @@
+'''
+Check Even Number and Second Last Digit is Two
+Write a function is_even_and_second_last_digit_is_two that takes an integer input num and returns a boolean value. The function should check if the number is even and if the second last digit is two. If both conditions are satisfied, return True; otherwise, return False.
+
+Examples
+
+>>> is_even_and_second_last_digit_is_two(232)
+False
+>>> is_even_and_second_last_digit_is_two(120)
+True
+>>> is_even_and_second_last_digit_is_two(25)
+False
+>>> is_even_and_second_last_digit_is_two(13)
+False
+'''
