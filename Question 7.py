2c4e81248d035ceac8d2fc00673362e00c5b1aec
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:42:20 +0530

    Create Question 7.py

---
+++ Question 7.py
@@ -0,0 +1,41 @@
+'''
+Pattern printing - Centered Triangle Of Zeroes
+Print a centered triangle up to a given number of rows, where all elements are zeroes. The number of rows is taken as input.
+
+There is space between zeros in a same line and also no space after the last zero in a line.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+A single integer n, representing the number of rows in the pattern.
+Output Format
+
+A centered triangle having n number of rows, where all elements are zeroes
+Examples
+
+Input:
+
+3
+Output:
+
+  0
+ 0 0
+0 0 0
+Input:
+
+2
+Output:
+
+ 0
+0 0
+Input:
+
+4
+Output:
+
+   0
+  0 0
+ 0 0 0
+0 0 0 0
+'''
