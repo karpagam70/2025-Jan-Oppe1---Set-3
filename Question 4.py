ec94ebe491a9a5dbe55bddc4f5c451e946745541
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:39:49 +0530

    Create Question 4.py

---
+++ Question 4.py
@@ -0,0 +1,6 @@
+'''
+Replace Vowels with Next Alphabet
+You are given a string s. Your task is to replace each vowel in the string with the next letter in the alphabet while keeping the rest of the characters unchanged.
+
+Note: The function is case-sensitive. For example, a is replaced with b, and A is replaced with B.
+'''
