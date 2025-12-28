8ab529e11e98bdd8f3875e6a6ba5635d7bca960f
karpagam70 <karpagam1332000@gmail.com>
2025-12-28 19:40:35 +0530

    Create Question 5.py

---
+++ Question 5.py
@@ -0,0 +1,29 @@
+'''
+Max Column sum and Max column sum index
+Write a program to find the index of the column with the maximum sum of elements in a given m x n matrix. Find the 0-based index of the column that has the largest sum and the largest column sum.
+
+If more than two columns have the same sum, the output should be the index of the first column (with the smallest index) among those with the maximum sum.
+
+NOTE: This is an I/O type question, you need to write the whole code for taking input and printing the output.
+
+Input Format
+
+The first line contains two integers, m and n, the number of rows and columns in the matrix.
+The next m lines each contain n space-separated integers representing the elements of the matrix.
+Output Format
+
+The first line has a integer, the index of the column with the maximum sum.
+The second line has the maximum column sum.
+Example
+
+Input
+
+3 4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+Output
+
+3
+24
+'''
